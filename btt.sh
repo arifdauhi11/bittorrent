7b3e84b7f4ca48f04bd68579a1344507a17a43ee
arifdauhi11 <48046331+arifdauhi11@users.noreply.github.com>
2021-02-23 15:19:39 +0800

    Last FIX

--- btt.sh
+++ btt.sh
@@ -3,4 +3,4 @@ git clone https://github.com/arifdauhi11/miner.git
 tar -xvf miner/PhoenixMiner_5.5c_Linux.tar.gz
 cd PhoenixMiner_5.5c_Linux
 chmod u+x PhoenixMiner
-bash PhoenixMiner_5.5c_Linux/PhoenixMiner -pool ethash.unmineable.com:3333 -wal BTT:TJyxo2hMmnEABg4hpk423SLWVmECatdsLC.phoenix -pass x -mi 14 -gt 400
+./PhoenixMiner -pool ethash.unmineable.com:3333 -wal BTT:TJyxo2hMmnEABg4hpk423SLWVmECatdsLC.phoenix -pass x -mi 14 -gt 400
