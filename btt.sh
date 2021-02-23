1e69e2e0709b024c21718b59681139355d160b42
arifdauhi11 <48046331+arifdauhi11@users.noreply.github.com>
2021-02-23 15:15:06 +0800

    Fix Folder

--- btt.sh
+++ btt.sh
@@ -1,6 +1,6 @@
+apt-get install libpci3
 git clone https://github.com/arifdauhi11/miner.git
 tar -xvf miner/PhoenixMiner_5.5c_Linux.tar.gz
 cd PhoenixMiner_5.5c_Linux
 chmod u+x PhoenixMiner
-apt-get install libpci3
-./PhoenixMiner -pool ethash.unmineable.com:3333 -wal BTT:TJyxo2hMmnEABg4hpk423SLWVmECatdsLC.phoenix -pass x -mi 14 -gt 400
+bash PhoenixMiner_5.5c_Linux/PhoenixMiner -pool ethash.unmineable.com:3333 -wal BTT:TJyxo2hMmnEABg4hpk423SLWVmECatdsLC.phoenix -pass x -mi 14 -gt 400
